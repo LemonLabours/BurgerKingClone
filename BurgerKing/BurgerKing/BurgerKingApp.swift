57e427dc85bf8ae548ac482976b0c8933afac7cd
Lemon <101458752+LemonLabours@users.noreply.github.com>
2023-07-07 16:59:42 +0300

    first commit

---
+++ BurgerKing/BurgerKing/BurgerKingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurgerKingApp.swift
+//  BurgerKing
+//
+//  Created by Lama AL Yousef on 06/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BurgerKingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BurgerKing/BurgerKing/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BurgerKing
+//
+//  Created by Lama AL Yousef on 06/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
