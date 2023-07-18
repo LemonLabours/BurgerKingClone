7b6689626eb5238b9ffff2af8887952ac97b7e96
Lemon <101458752+LemonLabours@users.noreply.github.com>
2023-07-18 22:33:33 +0300

    Added OffersView SubViews

---
+++ BurgerKing/BurgerKing/Views/SubViews/OffersComponents/AuthSheetView.swift
@@ -0,0 +1,20 @@
+//
+//  AuthSheetView.swift
+//  BurgerKing
+//
+//  Created by Lama AL Yousef on 18/07/2023.
+//
+
+import SwiftUI
+
+struct AuthSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AuthSheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthSheetView()
+    }
+}
