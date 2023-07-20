bf389263cea5e6ac9f05a0819f42648937c53cf5
Lemon <101458752+LemonLabours@users.noreply.github.com>
2023-07-20 17:42:42 +0300

    Updated SettingsView

--- BurgerKing/BurgerKing/Views/SubViews/ProfileCompnents/SettingsView.swift
+++ BurgerKing/BurgerKing/Views/SubViews/ProfileCompnents/SettingsView.swift
@@ -8,8 +8,111 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @Environment(\.presentationMode) var presentationMode // Get the presentationMode
+
     var body: some View {
-        Text("Settings")
+        ZStack {
+            Color.bkDarkBG
+                .ignoresSafeArea()
+            VStack {
+                CustomNavigationBar(title: "Settings", presentationMode: presentationMode) // Pass the presentationMode
+
+                VStack{
+                    HStack(){
+                        Text("Manage")
+                            .foregroundColor(Color.bkBrown)
+                        Spacer()
+                    }.padding(.leading, 32)
+                        .padding(.vertical, 16)
+
+
+                    Divider()
+                        .foregroundColor(Color.bkBrown)
+
+                        .padding(.horizontal)
+                    VStack (spacing: 32){
+                        HStack{
+                            Text("Location")
+                                .foregroundColor(Color.bkDarkBrown)
+                                .flame(font: .regular, size: adaptiveTextSize(size: 16, max: 32))
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(Color.bkBrown)
+                                .font(.subheadline)
+                        }
+
+                        HStack{
+                            Text("Notification")
+                                .foregroundColor(Color.bkDarkBrown)
+                                .flame(font: .regular, size: adaptiveTextSize(size: 16, max: 32))
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(Color.bkBrown)
+                                .font(.subheadline)
+                        }
+                    }.padding(.horizontal, 32)
+                        .padding(.vertical, 16)
+                }.background(Color.bkBG)
+
+                VStack{
+                    HStack(){
+                        Text("Legal")
+                            .foregroundColor(Color.bkBrown)
+                        Spacer()
+                    }.padding(.leading, 32)
+                        .padding(.vertical, 16)
+
+
+                    Divider()
+                        .foregroundColor(Color.bkBrown)
+
+                        .padding(.horizontal)
+                    VStack (spacing: 32){
+                        HStack{
+                            Text("Terms of services")
+                                .foregroundColor(Color.bkDarkBrown)
+                                .flame(font: .regular, size: adaptiveTextSize(size: 16, max: 32))
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(Color.bkBrown)
+                                .font(.subheadline)
+                        }
+
+                        HStack{
+                            Text("Privacy policy")
+                                .foregroundColor(Color.bkDarkBrown)
+                                .flame(font: .regular, size: adaptiveTextSize(size: 16, max: 32))
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(Color.bkBrown)
+                                .font(.subheadline)
+                        }
+                        HStack{
+                            Text("Acknowledgments")
+                                .foregroundColor(Color.bkDarkBrown)
+                                .flame(font: .regular, size: adaptiveTextSize(size: 16, max: 32))
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(Color.bkBrown)
+                                .font(.subheadline)
+                        }
+                    }.padding(.horizontal, 32)
+                        .padding(.vertical, 16)
+                }.background(Color.bkBG)
+
+                HStack{
+                    Text("V6.32.0")
+                        .foregroundColor(Color.bkBrown)
+                    Spacer()
+                }.padding(.horizontal, 32)
+                    .padding(.vertical, 16)
+                .background(Color.bkBG)
+
+                Spacer()
+
+
+            }
+        }.navigationBarBackButtonHidden()
     }
 }
 
