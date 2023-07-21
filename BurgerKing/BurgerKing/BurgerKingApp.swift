94911c3a4ee03f6ce203735adc63b7e2d0ed2fa9
Lemon <101458752+LemonLabours@users.noreply.github.com>
2023-07-21 21:52:40 +0300

    Completed Firebase Setup

--- BurgerKing/BurgerKing/BurgerKingApp.swift
+++ BurgerKing/BurgerKing/BurgerKingApp.swift
@@ -6,9 +6,26 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+    FirebaseApp.configure()
+
+    return true
+
+  }
+
+}
 
 @main
 struct BurgerKingApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject private var locationViewModel = AllowLocationViewModel()
 
     var body: some Scene {
---
+++ BurgerKing/BurgerKing/Managers/Authentication/AuthenticationManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationManager.swift
+//  BurgerKing
+//
+//  Created by Lama AL Yousef on 21/07/2023.
+//
+
+import Foundation
