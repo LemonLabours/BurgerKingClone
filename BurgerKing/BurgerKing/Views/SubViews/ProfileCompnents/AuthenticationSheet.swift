5b6cc34ae0a37ba58632b6cbd646d765ed4f2c01
Lemon <101458752+LemonLabours@users.noreply.github.com>
2023-07-21 21:48:08 +0300

    Add Navigation from AuthenticationSheet

--- BurgerKing/BurgerKing/Views/SubViews/ProfileCompnents/AuthenticationSheet.swift
+++ BurgerKing/BurgerKing/Views/SubViews/ProfileCompnents/AuthenticationSheet.swift
@@ -4,98 +4,102 @@
 //
 //  Created by Lama AL Yousef on 21/07/2023.
 //
-
 import SwiftUI
+
 
 struct AuthenticationSheet: View {
     var body: some View {
-        ZStack {
-            Color.bkBG
-                .ignoresSafeArea()
-            ScrollView{
-                VStack(spacing: 16) {
-                    Text("Create account or sign in to proceed")
-                        .foregroundColor(Color.bkBrown)
-                        .font(.subheadline)
-                    Button(action: {
-                        // Handle Facebook sign-in action here
-                    }) {
-                        HStack {
-                            Image("facebook-white")
-                                .resizable()
-                                .frame(width: 24, height: 24)
-                            Text("Continue with Facebook")
-                                .foregroundColor(Color.white)
-                                .flame(font: .regular, size: adaptiveTextSize(size: 22, max: 32))
+        NavigationView {
+            ZStack {
+                Color.bkBG
+                    .ignoresSafeArea()
+                ScrollView {
+                    VStack(spacing: 16) {
+                        Text("Create account or sign in to proceed")
+                            .foregroundColor(Color.bkBrown)
+                            .font(.subheadline)
+                        Button(action: {
+                            // Handle Facebook sign-in action here
+                        }) {
+                            HStack {
+                                Image("facebook-white")
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                Text("Continue with Facebook")
+                                    .foregroundColor(Color.white)
+                                    .flame(font: .regular, size: adaptiveTextSize(size: 22, max: 32))
+                            }
+                            .padding(.vertical, 16)
+                            .frame(maxWidth: .infinity) // Set the width to infinity
+                            .background(Color.blue)
+                            .cornerRadius(8)
                         }
-                        .padding(.vertical, 16)
-                        .frame(maxWidth: .infinity) // Set the width to infinity
-                        .background(Color.blue)
-                        .cornerRadius(8)
-                    }
 
-                    Button(action: {
-                        // Handle Apple sign-in action here
-                    }) {
-                        HStack {
-                            Image(systemName: "applelogo")
-                                .foregroundColor(Color.white)
-                                .font(.title)
-                            Text("Continue with Apple")
-                                .foregroundColor(Color.white)
-                                .flame(font: .regular, size: adaptiveTextSize(size: 22, max: 32))
+                        Button(action: {
+                            // Handle Apple sign-in action here
+                        }) {
+                            HStack {
+                                Image(systemName: "applelogo")
+                                    .foregroundColor(Color.white)
+                                    .font(.title)
+                                Text("Continue with Apple")
+                                    .foregroundColor(Color.white)
+                                    .flame(font: .regular, size: adaptiveTextSize(size: 22, max: 32))
+                            }
+                            .padding(16)
+                            .frame(maxWidth: .infinity) // Set the width to infinity
+                            .background(Color.black)
+                            .cornerRadius(8)
                         }
-                        .padding(16)
-                        .frame(maxWidth: .infinity) // Set the width to infinity
-                        .background(Color.black)
-                        .cornerRadius(8)
-                    }
-                    
 
-                    Button(action: {
-                        // Handle email sign-in action here
-                    }) {
-                        HStack {
-                            Text("Continue with Email")
-                                .foregroundColor(Color.white)
-                                .flame(font: .regular, size: adaptiveTextSize(size: 22, max: 32))
+                        Button(action: {
+                            // Handle email sign-in action here
+                        }) {
+                            HStack {
+                                Text("Continue with Email")
+                                    .foregroundColor(Color.white)
+                                    .flame(font: .regular, size: adaptiveTextSize(size: 22, max: 32))
+                            }
+                            .padding(16)
+                            .frame(maxWidth: .infinity) // Set the width to infinity
+                            .background(Color.bkRed)
+                            .cornerRadius(8)
                         }
-                        .padding(16)
-                        .frame(maxWidth: .infinity) // Set the width to infinity
-                        .background(Color.bkRed)
-                        .cornerRadius(8)
 
-                    }
-                    VStack (spacing: 3){
-                        HStack(spacing: 3){
-                            Text("By proceeding, you accept our") .foregroundColor(Color.bkBrown)   .font(.caption2)
-                            Button{
+                        VStack(spacing: 3) {
+                            HStack(spacing: 3) {
+                                Text("By proceeding, you accept our")
+                                    .foregroundColor(Color.bkBrown)
+                                    .font(.caption2)
 
-                            }label: {
-                                Text("Terms of service ")
-                                    .underline() .foregroundColor(Color.bkDarkBrown)  .flame(font: .regular, size: 10)
+                                NavigationLink(destination: TermsOfServiceView()) {
+                                    Text("Terms of service")
+                                        .underline()
+                                        .foregroundColor(Color.bkDarkBrown)
+                                        .flame(font: .regular, size: 10)
+                                }
                             }
 
-                        }
-                        HStack(spacing: 3){
-                            Text("and confirm that you have read our") .foregroundColor(Color.bkBrown).font(.caption2)
-                            Button{
+                            HStack(spacing: 3) {
+                                Text("and confirm that you have read our")
+                                    .foregroundColor(Color.bkBrown)
+                                    .font(.caption2)
 
-                            }label: {
-                                Text("Privacy policy.")
-                                    .underline() .foregroundColor(Color.bkDarkBrown) .flame(font: .regular, size: 10)
+                                NavigationLink(destination: PrivacyPolicyView()) {
+                                    Text("Privacy policy")
+                                        .underline()
+                                        .foregroundColor(Color.bkDarkBrown)
+                                        .flame(font: .regular, size: 10)
+                                }
                             }
-
                         }
 
+                        Spacer()
                     }
-
-                    Spacer()
+                    .padding(.horizontal, 32)
+                    .padding(.vertical, 16)
                 }
-                .padding(.horizontal, 32)
-                .padding(.vertical, 16)
             }
-
         }
     }
 }
