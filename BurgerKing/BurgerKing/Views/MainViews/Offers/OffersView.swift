5267862900f944558fcd6fa7f0dda2f6045f9fad
Lemon <101458752+LemonLabours@users.noreply.github.com>
2023-07-30 13:07:16 +0300

    Updated Offers View

--- BurgerKing/BurgerKing/Views/MainViews/Offers/OffersView.swift
+++ BurgerKing/BurgerKing/Views/MainViews/Offers/OffersView.swift
@@ -12,11 +12,52 @@ struct OffersView: View {
         ZStack{
             Color.bkBG
                 .ignoresSafeArea()
-            Text("Offers")
 
+            VStack{
+                HStack{
+                    Text("Offers")
+                    Spacer()
+                }.padding(20)
+                    .foregroundColor(Color.bkDarkBrown)
+                    .flame(font: .bold, size: adaptiveTextSize(size: 28, max: 48))
+
+
+                Text("Your offers will show up here. We'll inform \nyou when new ones are available.").padding(.horizontal,20)
+                    .foregroundColor(Color.bkBrown)
+                    .font(.caption)
+                    .bold()
+
+
+                Spacer()
+                HStack{
+                    Spacer()
+
+                    Button {
+                    }label: {
+                         HStack {
+                            Image(systemName: "gift.fill")
+                                .foregroundColor(.white)
+                                .font(.title2)
+                            Text("Enter Promo Code")
+                                .foregroundColor(.white)
+                                .font(.caption)
+                                .bold()
+
+                         }.padding()
+                         .background(Color.bkRed)
+                         .cornerRadius(35)
+                         .padding()
+                         }
+
+
+                     }
+                  }
+
+               }
+
+           }
         }
-    }
-}
+
 
 struct OffersView_Previews: PreviewProvider {
     static var previews: some View {
